d2722489af67a20ae5852e288859821603e7ec9a
John Exim <john.exim.ece@skyland.tw>
2020-06-06 17:36:05 +0800

    Added show_hand and add_to_hand methods in Dealer class

--- dealer.py
+++ dealer.py
@@ -5,6 +5,7 @@ class Dealer(object):
 
     def __init__(self, deck):
         self.deck = deck
+        self.hand = []
 
         # pre-shuffle deck upon instantiation
         self.shuffle_deck()
@@ -15,6 +16,13 @@ class Dealer(object):
     def draw_card(self):
         return self.deck.pop()
 
+    def add_to_hand(self, *cards):
+        for card in cards:
+            self.hand.append(card)
+
+    def show_hand(self):
+        return self.hand
+
     @staticmethod
     def evaluate_hand_blackjack(hand):
         hand_value: int = 0
@@ -22,12 +30,13 @@ class Dealer(object):
         for card in hand:
             if card[0] == 'ace':
                 aces.append(card)
-                hand_value += 1
-            elif card[0] in ['jack','queen','king']:
+                hand_value += 1  # consider all aces as having a value of 1, initially.
+            elif card[0] in ['jack', 'queen', 'king']:
                 hand_value += 10
             else:
                 hand_value += card[0]
 
+        # for each ace in hand, add 10 to hand_value while hand_value is less than 12, so as not to go bust/over 21.
         if aces:
             for _ in aces:
                 while hand_value < 12:
