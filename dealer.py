218c63b839cbfec170a7bc47969e164aa9eb609d
John Exim <john.exim.ece@skyland.tw>
2020-06-06 14:52:34 +0800

    Added evaluation for blackjack hand and corresponding unit test

--- dealer.py
+++ dealer.py
@@ -6,7 +6,7 @@ class Dealer(object):
     def __init__(self, deck):
         self.deck = deck
 
-        #pre-shuffle deck upon instantiation
+        # pre-shuffle deck upon instantiation
         self.shuffle_deck()
 
     def shuffle_deck(self):
@@ -14,3 +14,23 @@ class Dealer(object):
 
     def draw_card(self):
         return self.deck.pop()
+
+    @staticmethod
+    def evaluate_hand_blackjack(hand):
+        hand_value: int = 0
+        aces = []
+        for card in hand:
+            if card[0] == 'ace':
+                aces.append(card)
+                hand_value += 1
+            elif card[0] in ['jack','queen','king']:
+                hand_value += 10
+            else:
+                hand_value += card[0]
+
+        if aces:
+            for _ in aces:
+                while hand_value < 12:
+                    hand_value += 10
+
+        return hand_value
---
+++ test_hand_evaluation.py
@@ -0,0 +1,32 @@
+import unittest
+
+from dealer import Dealer
+
+
+class TestBlackJack(unittest.TestCase):
+
+    def test_int_hand(self):
+        hand = [(3, 'hearts'), (5, 'spades')]
+        self.assertEqual(Dealer.evaluate_hand_blackjack(hand), 8)
+
+    def test_faces_hand(self):
+        hand = [('jack', 'hearts'), ('king', 'spades'), ('queen', 'spades')]
+        self.assertEqual(Dealer.evaluate_hand_blackjack(hand), 30)
+
+    def test_aces_hand(self):
+        hand = [('ace', 'hearts'), ('ace', 'spades'), ('ace', 'spades')]
+        self.assertEqual(Dealer.evaluate_hand_blackjack(hand), 13)
+
+    def test_aceblackjack_hand(self):
+        hand = [('ace', 'hearts'), ('king', 'diamonds')]
+        self.assertEqual(Dealer.evaluate_hand_blackjack(hand), 21)
+
+    def test_acecombi_hand(self):
+        hand = [('ace', 'hearts'), ('king', 'diamonds'), ('ace', 'clubs')]
+        self.assertEqual(Dealer.evaluate_hand_blackjack(hand), 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
